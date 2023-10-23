3f161286a3e27b01cf8ac442a1ffc9ac54986f59
Sven Brettschneider <brettschneider.sven@gmail.com>
2023-10-23 12:29:57 +0200

    setting up test project

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use fluent::{FluentResource, FluentBundle, FluentArgs, FluentValue};
+use unic_langid::LanguageIdentifier;
+
+fn main() {
+    let ftl_string = String::from(r#"
+just-url-dynamic = content before link<a href="{$url}">this is a url</a>content after link
+link-dynamic = content before link{$url}content after link"#);
+    let res = FluentResource::try_new(ftl_string)
+        .expect("Failed to parse an FTL string.");
+    
+    let langid_en: LanguageIdentifier = "en-US".parse().expect("Parsing failed");
+    let mut bundle = FluentBundle::new(vec![langid_en]);
+    
+    bundle
+        .add_resource(res)
+        .expect("Failed to add FTL resources to the bundle.");
+
+    // Testing what should be two identical ftl outputs strings.
+
+    // Starting with a sentence with a url passed in. 
+    let mut just_url_dynamic_arg = FluentArgs::new();
+    just_url_dynamic_arg.set("url", FluentValue::from("http://www.google.com"));
+    
+    let msg = bundle.get_message("just-url-dynamic")
+        .expect("Message doesn't exist.");
+    let mut errors = vec![];
+    let pattern = msg.value().expect("Message has no value.");
+    let just_url_dynamic_value = bundle.format_pattern(&pattern, Some(&just_url_dynamic_arg), &mut errors);
+
+    // Then instead of passing in the url, pass in the entire html link.
+    let mut link_dynamic_arg = FluentArgs::new();
+    link_dynamic_arg.set("url", FluentValue::from(r#"<a href=http://www.google.com>this is a url</a>"#));
+
+    let msg = bundle.get_message("link-dynamic")
+    .expect("Message doesn't exist.");
+    let mut errors = vec![];
+    let pattern = msg.value().expect("Message has no value.");
+    let link_dynamic_value = bundle.format_pattern(&pattern, Some(&link_dynamic_arg), &mut errors);
+    
+    // both indeed look the same.
+    println!("{link_dynamic_value}\n{just_url_dynamic_value}");
+    println!("But are they? See for yourself: {}", link_dynamic_value == just_url_dynamic_value);
+
+    // They aren't the same. What does it actually look like?
+    println!("{:?}\n{:?}", link_dynamic_value.as_bytes(), just_url_dynamic_value.as_bytes());
+
+    // YUP they indeed are different!
+}
